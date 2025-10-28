d6dab9e0ce516c53d796576c7698581c30ad859d
Efimenko_Sergey <fima.ysa@mail.ru>
2025-10-28 15:37:19 +0300

    feat: npm install -D prettier-plugin-classnames for wrap className

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 function App() {
   return (
     <>
-      <div className='mx-3 h-20 w-20 rotate-45 border-b bg-yellow-100 py-5 text-center text-3xl text-cyan-800'>
+      <div
+        className='mx-3 h-20 w-20 rotate-45 border-b bg-yellow-100 py-5 text-center text-3xl
+          text-cyan-800'
+      >
         hi
       </div>
     </>
