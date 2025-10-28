769f3420ca2efe1eb236b9f995a68a4466c64316
Efimenko_Sergey <fima.ysa@mail.ru>
2025-10-28 15:19:00 +0300

    feat: npm install -D tailwindcss @tailwindcss/vite

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='border-b w-20 h-20 text-3xl text-center bg-yellow-100 text-cyan-800 rotate-45 mx-3 py-5'>
+        hi
       </div>
     </>
   )
