0d957b38a3c2a2c20604586893d2cc40b838ab9a
Efimenko_Sergey <fima.ysa@mail.ru>
2025-11-11 17:55:50 +0300

    feat: add adaptive section WhuUs to 320px

--- src/data/whyUs.ts
+++ src/data/whyUs.ts
@@ -2,39 +2,31 @@ export const whyUs = {
   title: 'Почему мы',
   background: {
     paper_BG: '/images/background/paper_BG.svg',
-    hop_parallax_BG: '/images/background/hop_parallax_BG.jpg'
+    hop_parallax_BG: '/images/background/hop_parallax_BG.jpg',
   },
   itemsArr: [
     {
       title: 'Отборный солод',
       svgImg: {
         url: '/images/whyUs/malt.svg',
-        width: '106px',
-        height: '185px',
       },
     },
     {
       title: 'Качественный хмель',
       svgImg: {
         url: '/images/whyUs/hop.svg',
-        width: '140px',
-        height: '171px',
       },
     },
     {
       title: 'Специальная вода',
       svgImg: {
         url: '/images/whyUs/water.svg',
-        width: '158px',
-        height: '186px',
       },
     },
     {
       title: 'Современное оборудование',
       svgImg: {
         url: '/images/whyUs/equipment.svg',
-        width: '285px',
-        height: '180px',
       },
     },
   ],
--- src/sections/home/SectionWhyUs.tsx
+++ src/sections/home/SectionWhyUs.tsx
@@ -6,22 +6,33 @@ export function SectionWhyUs() {
     <>
       <section
         style={{ backgroundImage: `url(${whyUs.background.paper_BG})` }}
-        className='h-[589px] bg-no-repeat bg-center pt-14'
+        className='bg-no-repeat bg-center bg-cover pt-9 md:pt-14 pb-19 md:pb-32.75'
       >
         <TitleSection title={whyUs.title} color='#000' position='center' />
-        <div className='flex gap-25 justify-center mt-12'>
+        <div
+          className='flex flex-wrap gap-y-13.75 gap-x-8 lg:gap-25 justify-center mt-3.25
+            lg:mt-11.75'
+        >
           {whyUs.itemsArr.map((el) => {
             return (
               <div key={el.title} className='flex flex-col items-center'>
-                <div className='h-[187px] relative flex justify-center items-end'>
-                  <div className='w-35.5 h-35.5 rounded-full bg-[#FDCF55] absolute bottom-[5px]'></div>
+                <div className='relative flex justify-center items-end'>
+                  <div
+                    className='size-23.75 lg:size-35.5 rounded-full bg-[#FDCF55] absolute
+                      bottom-[5px]'
+                  ></div>
                   <img
                     src={el.svgImg.url}
                     alt={el.title}
-                    className={`w-[${el.svgImg.width}] h-[${el.svgImg.height}] z-50`}
+                    className={'h-31 w-30 lg:h-auto lg:w-auto' + ' z-50'}
                   />
                 </div>
-                <p className='mt-12 w-45 text-2xl font-medium text-center'>{el.title}</p>
+                <p
+                  className='mt-5.5 w-30 lg:w-45 text-base leading-4.5 lg:mt-11.5 lg:text-2xl
+                    lg:leading-7.25 font-medium text-center'
+                >
+                  {el.title}
+                </p>
               </div>
             )
           })}
@@ -29,10 +40,13 @@ export function SectionWhyUs() {
       </section>
       <div
         style={{ backgroundImage: `url(${whyUs.background.hop_parallax_BG})` }}
-        className='h-65 bg-no-repeat bg-center'
+        className='bg-no-repeat bg-center bg-cover'
       >
-        <div className='h-full bg-black/40 flex justify-center items-center'>
-          <p className='text-[40px] text-white font-medium max-w-128 text-center'>
+        <div className='bg-black/40 flex justify-center items-center py-13.75 md:pt-21.5 md:pb-19.5'>
+          <p
+            className='text-xl leading-6 md:text-[40px] md:leading-12 text-white font-medium
+              max-w-128 text-center'
+          >
             “Чудесное пиво, для замечательных людей”
           </p>
         </div>
