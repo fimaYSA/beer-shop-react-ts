31f21b1438e0659d8d60edf006f14b9f25b07ed7
Efimenko_Sergey <fima.ysa@mail.ru>
2025-11-17 14:00:37 +0300

    feat: add BgPaper.tsx

---
+++ src/components/containers/BgPaper.tsx
@@ -0,0 +1,11 @@
+import type { ReactElement } from 'react'
+
+type Props = {
+  children: ReactElement
+}
+
+export const BgPaper = ({ children }: Props) => (
+  <section className='bg-[url(images/background/paper_BG.svg)] bg-center px-3.75'>
+    <div className='mx-auto max-w-[1120px]'>{children}</div>
+  </section>
+)
