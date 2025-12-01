e2b20ae2265513bd3524d085ae016361c264f0fd
Efimenko_Sergey <fima.ysa@mail.ru>
2025-12-01 18:32:56 +0300

    fix base url-3

--- src/components/containers/Background.tsx
+++ src/components/containers/Background.tsx
@@ -1,4 +1,5 @@
 import type { ReactNode } from 'react'
+import { cn } from '@sglara/cn'
 
 type Props = {
   children: ReactNode
@@ -7,14 +8,19 @@ type Props = {
 }
 
 export const Background = ({ background, blackOut = 0, children }: Props) => {
+  const baseUrl = import.meta.env.BASE_URL
   let style
   if (background === 'hop')
-    style = 'bg-[url(images/background/hop_parallax.jpg)] bg-no-repeat  bg-center bg-cover'
-  if (background === 'paper') style = 'bg-[url(images/background/paper_BG.svg)] bg-center'
-  if (background === 'white') style = 'bg-white'
+    style = { backgroundImage: `url(${baseUrl}images/background/hop_parallax.jpg` }
+  if (background === 'paper')
+    style = { backgroundImage: `url(${baseUrl}images/background/paper_BG.svg` }
+  if (background === 'white') style = { backgroundColor: 'white' }
 
   return (
-    <section className={style}>
+    <section
+      style={style}
+      className={cn([background === 'hop' && 'bg-cover bg-no-repeat'], 'bg-center')}
+    >
       <div className={`px-3.75 bg-black/${blackOut}`}>
         <div className='mx-auto max-w-[1110px]'>{children}</div>
       </div>
